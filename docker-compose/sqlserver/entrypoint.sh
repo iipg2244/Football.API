1950f0c1cc1edc578697ed633c5611145f8617ca
Isaac Portilla <66019424+iipg2244@users.noreply.github.com>
2024-10-22 17:11:44 +0200

    Add FULLTEXTSERVICEPROPERTY.

--- docker-compose/sqlserver/entrypoint.sh
+++ docker-compose/sqlserver/entrypoint.sh
@@ -1,2 +1,16 @@
-# Run Microsoft SQl Server and initialization script (at the same time)
-/usr/src/app/run-initialization.sh & /opt/mssql/bin/sqlservr
+#!/bin/bash
+
+# Start SQL Server
+/opt/mssql/bin/sqlservr &
+
+# Wait for SQL Server to start
+sleep 30s
+
+host="$(hostname --ip-address || echo '127.0.0.1')"
+user=SA
+password=$MSSQL_SA_PASSWORD
+
+/opt/mssql-tools/bin/sqlcmd -S $host -U $user -P $password -C -i /usr/src/app/init.sql
+
+# Wait indefinitely to keep the container running
+wait
